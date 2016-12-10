fac3ca44674b3f83a64e7f89e505cdf13c1b7ce9
njzarrilli <njzarrilli@gmail.com>
2016-12-10 17:00:19 -0500

    more scikitlearn stuff

--- build_model.py
+++ build_model.py
@@ -1,17 +1,48 @@
+import re
+from sklearn import linear_model
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
 def train_model(training_data, training_results):
     pass
 
-def get_held_out_data(lines):
-    pass
 
 def get_training_data(lines):
-    pass
+    train_data_lines = []
+    train_result_lines = []
+
+    for line in lines[1:]:
+        line_data = re.split(r'\t+', line)
+        essay = line_data[2].encode('utf8')
+        essay_score = line_data[5]
+        train_data_lines.append(essay)
+        train_result_lines.append(essay_score)
+    
+    return (train_data_lines, train_result_lines)
 
 def main():
     f = open("training_set_rel3.tsv")
     lines = list(f)
-    train_data_lines, train_result_lines = get_training_data(lines)
-    held_out_data_lines, held_out_result_lines = get_held_out_data(lines)
+    train_data_essays, train_data_scores = get_training_data(lines)
+    print(train_data_essays[0])
+    print(train_data_essays[0][79])
+    #print(train_data_scores[:2])
+   
+
+    count_vect = CountVectorizer(min_df=1)
+    X_train_counts = count_vect.fit_transform(train_data_essays)
+    print(count_vect.vocabulary_.get(u'algorithm'))
+
+    tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+    X_train_tf = tf_transformer.transform(X_train_counts)
+    print(X_train_tf.shape)
+
+    best_model = linear_model.LogisticRegression(tol=.000001)
+    best_model.fit(training_dataset, train_result_lines)
+
 
     #traing model on train data and test it on the held out data set
-    best_model = train_model(train_data_lines, train_result_lines)
+    best_model = train_model(train_data_essays, train_data_scores)
+
+main()
