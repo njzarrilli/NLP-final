54768cf065d3a2781088611ac3109460891cd93a
njzarrilli <njzarrilli@gmail.com>
2016-12-10 17:13:38 -0500

    ignoring unicode in data

--- build_model.py
+++ build_model.py
@@ -14,7 +14,8 @@ def get_training_data(lines):
 
     for line in lines[1:]:
         line_data = re.split(r'\t+', line)
-        essay = line_data[2].encode('utf8')
+        line_data[2] = line_data[2].decode('unicode_escape').encode('ascii','ignore')
+        essay = line_data[2].encode('utf-8')
         essay_score = line_data[5]
         train_data_lines.append(essay)
         train_result_lines.append(essay_score)
@@ -26,20 +27,19 @@ def main():
     lines = list(f)
     train_data_essays, train_data_scores = get_training_data(lines)
     print(train_data_essays[0])
-    print(train_data_essays[0][79])
     #print(train_data_scores[:2])
    
 
     count_vect = CountVectorizer(min_df=1)
     X_train_counts = count_vect.fit_transform(train_data_essays)
-    print(count_vect.vocabulary_.get(u'algorithm'))
+    print("hello")
+    print(X_train_counts.shape)
 
     tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
     X_train_tf = tf_transformer.transform(X_train_counts)
     print(X_train_tf.shape)
 
     best_model = linear_model.LogisticRegression(tol=.000001)
-    best_model.fit(training_dataset, train_result_lines)
 
 
     #traing model on train data and test it on the held out data set
